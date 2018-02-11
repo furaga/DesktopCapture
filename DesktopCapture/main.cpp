c9253f031e7992fd537c0fb7c645731280fa7f72
DESKTOP-5J7ES5Q\furaga <furaga.fukahori@gmail.com>
2018-02-12 03:46:24 +0900

    WIP

--- DesktopCapture/main.cpp
+++ DesktopCapture/main.cpp
@@ -19,7 +19,12 @@ void Printf(const char *format, ...)
 	va_end(args);
 }
 
-int captureAndSaveImage(HWND hwnd, HDC hdc, char* fileName) {
+HDC hmdc = nullptr;
+HBITMAP hbmp = nullptr;
+BYTE* line = nullptr;
+UINT sizeOfLine = 0;
+
+int captureAndSaveImage(HWND hwnd, HDC hdc, char* fileName, bool saveFile = true) {
 	const BYTE bfh[14] = { 0x42, 0x4d, 0,0,0,0, 0,0, 0,0, 0x36,0,0,0 };
 
 	// capture image
@@ -28,23 +33,23 @@ int captureAndSaveImage(HWND hwnd, HDC hdc, char* fileName) {
 	UINT width = rect.right - rect.left;
 	UINT height = rect.bottom - rect.top;
 
-	HDC hmdc = CreateCompatibleDC(hdc);
-	HBITMAP hbmp = CreateCompatibleBitmap(hdc, width, height);
+	if (hmdc == nullptr) {
+		hmdc = CreateCompatibleDC(hdc);
+		hbmp = CreateCompatibleBitmap(hdc, width, height);
+	}
 
 	HBITMAP hbmpOld = (HBITMAP)SelectObject(hmdc, hbmp);
 	BitBlt(hmdc, 0, 0, width, height, hdc, 0, 0, SRCCOPY);
 	SelectObject(hmdc, hbmpOld);
 
-	UINT sizeOfLine = width * 3;
-	sizeOfLine += (sizeOfLine % 4 != 0 ? 4 - sizeOfLine % 4 : 0);
-	BYTE* line = (BYTE*)malloc(sizeOfLine);
-
-	// save as bitmap image
-	FILE* file;
-	fopen_s(&file, fileName, "wb");
-
-	*((DWORD*)(&bfh[2])) = 0x36 + sizeOfLine * height;
-	fwrite(bfh, sizeof bfh, 1, file);
+	if (line == nullptr) {
+		sizeOfLine = width * 3;
+		sizeOfLine += (sizeOfLine % 4 != 0 ? 4 - sizeOfLine % 4 : 0);
+		if (!saveFile) {
+			sizeOfLine *= height;
+		}
+		line = (BYTE*)malloc(sizeOfLine);
+	}
 
 	BITMAPINFO bi;
 	ZeroMemory(&bi, sizeof bi);
@@ -55,17 +60,33 @@ int captureAndSaveImage(HWND hwnd, HDC hdc, char* fileName) {
 	bi.bmiHeader.biBitCount = 24;
 	bi.bmiHeader.biCompression = BI_RGB;
 	bi.bmiHeader.biSizeImage = sizeOfLine * height;
-	fwrite(&(bi.bmiHeader), sizeof bi.bmiHeader, 1, file);
 
-	for (unsigned int i = 0; i < height; i++) {
-		GetDIBits(hmdc, hbmp, i, 1, line, &bi, DIB_RGB_COLORS);
-		fwrite(line, sizeOfLine, 1, file);
+	// save as bitmap image
+	FILE* file = nullptr;
+	if (saveFile) {
+		fopen_s(&file, fileName, "wb");
+
+		*((DWORD*)(&bfh[2])) = 0x36 + sizeOfLine * height;
+		fwrite(bfh, sizeof bfh, 1, file);
+		fwrite(&(bi.bmiHeader), sizeof bi.bmiHeader, 1, file);
 	}
 
-	fclose(file);
-	free(line);
-	DeleteObject(hbmp);
-	DeleteDC(hmdc);
+	if (saveFile) {
+		for (unsigned int i = 0; i < height; i++) {
+			GetDIBits(hmdc, hbmp, i, 1, line, &bi, DIB_RGB_COLORS);
+			fwrite(line, sizeOfLine, 1, file);
+		}
+	}
+	else {
+		GetDIBits(hmdc, hbmp, 0, height, line, &bi, DIB_RGB_COLORS);
+	}
+
+	if (saveFile) {
+		fclose(file);
+	}
+	//free(line);
+	//DeleteObject(hbmp);
+	//DeleteDC(hmdc);
 	return 0;
 }
 
@@ -73,8 +94,8 @@ int WINAPI WinMain(HINSTANCE hCurInst, HINSTANCE hPrevInst, LPSTR lpsCmdLine, in
 {
 	MSG msg;
 	char fileName[1024];
-	HWND hwnd;
-	HDC hdc;
+	HWND hwnd = nullptr;
+	HDC hdc = nullptr;
 	int n = 0;
 
 	// set shortcut keys
@@ -83,29 +104,43 @@ int WINAPI WinMain(HINSTANCE hCurInst, HINSTANCE hPrevInst, LPSTR lpsCmdLine, in
 	RegisterHotKey(NULL, 2, MOD_ALT, 0x2C); // Alt + Print screen
 	RegisterHotKey(NULL, 3, 0, 0x1B); // Escape
 
+	clock_t begin, end;
+	double elapsed_secs;
+
 	while (GetMessage(&msg, NULL, 0, 0)) {
 		if (msg.message == WM_HOTKEY) {
 			switch (msg.wParam) {
 			case  1:
 				// print screen -> capture whole desktop window
-				hwnd = GetDesktopWindow();
-				hdc = GetDC(NULL);
+				if (hwnd == nullptr) {
+					hwnd = GetDesktopWindow();
+					hdc = GetDC(NULL);
+				}
 				sprintf_s(fileName, "%s%03d.bmp", lpsCmdLine, n);
-				captureAndSaveImage(hwnd, hdc, fileName);
-				ReleaseDC(NULL, hdc);
+
+
+				begin = clock();
+				for (int i = 0; i < 100; i++) {
+					captureAndSaveImage(hwnd, hdc, fileName, false);
+				}
+				end = clock();
+				elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+				Printf("elapsed time = %f s | average = %f s", elapsed_secs, elapsed_secs / 100);
+
+				// ReleaseDC(NULL, hdc);
 				n++;
 				Printf("captured desktop window: %s", fileName);
 				break;
 			case  2:
-				// Alt + print screen -> capture foreground window
-				hwnd = GetForegroundWindow();
-				hdc = GetWindowDC(hwnd);
-				sprintf_s(fileName, "%s%03d.bmp", lpsCmdLine, n);
-				captureAndSaveImage(hwnd, hdc, fileName);
-				ReleaseDC(hwnd, hdc);
-				n++;
-				Printf("captured foreground window: %s", fileName);
-				break;
+				//// Alt + print screen -> capture foreground window
+				//hwnd = GetForegroundWindow();
+				//hdc = GetWindowDC(hwnd);
+				//sprintf_s(fileName, "%s%03d.bmp", lpsCmdLine, n);
+				//captureAndSaveImage(hwnd, hdc, fileName);
+				//ReleaseDC(hwnd, hdc);
+				//n++;
+				//Printf("captured foreground window: %s", fileName);
+				//break;
 			case 3:
 				// Escape -> Quit application
 				PostQuitMessage(0);
